115e9a32d241fb2a5fa297d0b4d24b2cfe84f708
cefeher <cefeher@gmail.com>
2023-04-23 18:23:35 +0200

    Pantalla login funcional

---
+++ app/src/main/java/com/example/trainerapp/AuthActivity.kt
@@ -0,0 +1,80 @@
+package com.example.trainerapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.example.trainerapp.databinding.ActivityAuthBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class AuthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setup()
+    }
+
+    private fun setup() {
+        title = "Autenticación"
+        binding.signInButton.setOnClickListener {
+            if (binding.emailEditText.text.isNotEmpty() && binding.passwordEditText.text.isNotEmpty()) {
+                val password = binding.passwordEditText.text.toString()
+                if (password.length >= 6) {
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                        binding.emailEditText.text.toString(),
+                        binding.passwordEditText.text.toString()
+                    ).addOnCompleteListener {
+                        try {
+                            if (it.isSuccessful) {
+                                showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                            } else {
+                                showAlert("El correo electrónico ya está en uso")
+                            }
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                            showAlert("Error al crear el usuario")
+                        }
+                    }
+                } else {
+                    showAlert("La contraseña debe tener al menos 6 caracteres")
+                }
+            }
+        }
+
+        binding.logInButton.setOnClickListener {
+            if (binding.emailEditText.text.isNotEmpty() && binding.passwordEditText.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                    binding.emailEditText.text.toString(),
+                    binding.passwordEditText.text.toString()
+                ).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert("Error al iniciar sesión")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showAlert(message: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage(message)
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showHome(email: String, provider: ProviderType) {
+        val homeIntent: Intent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
+    }
+}
